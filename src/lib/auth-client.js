cddc408c551b0cf7e43383bc80bec11f5183eabe
tanvir1310 <tanvir171728@gmail.com>
2026-05-05 13:26:00 +0600

    updated

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
   baseURL: "https://ass-08-b13.vercel.app",
 });
 export const { signin, signup } = createAuthClient();
